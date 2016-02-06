bc172c7d1dff6f4b57a4ceed68d7ec3f1af0d60d
Balaji Chellappa <balaji.srgm@gmail.com>
2016-02-06 15:54:09 -0500

    Git Test

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -8,7 +8,7 @@ import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
   directives: [MATERIAL_DIRECTIVES]
 })
 export class HomeCmp {
-    
+name = "Balaji";    
 states = [
     'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME',
     'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA',
